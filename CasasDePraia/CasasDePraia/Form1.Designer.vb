f51a389639a8dd379213625994cf4053bbb368d8
Ricardo Velozo <rvelozo@msn.com>
2015-01-18 21:08:34 -0200

    Calendars

--- CasasDePraia/CasasDePraia/Form1.Designer.vb
+++ CasasDePraia/CasasDePraia/Form1.Designer.vb
@@ -22,17 +22,218 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim Group1 As Infragistics.Win.UltraWinListBar.Group = New Infragistics.Win.UltraWinListBar.Group(True)
+        Dim Group2 As Infragistics.Win.UltraWinListBar.Group = New Infragistics.Win.UltraWinListBar.Group()
+        Dim Group3 As Infragistics.Win.UltraWinListBar.Group = New Infragistics.Win.UltraWinListBar.Group()
+        Dim Group4 As Infragistics.Win.UltraWinListBar.Group = New Infragistics.Win.UltraWinListBar.Group()
+        Dim Group5 As Infragistics.Win.UltraWinListBar.Group = New Infragistics.Win.UltraWinListBar.Group()
+        Dim TimeInterval1 As Infragistics.Win.UltraWinSchedule.TimeInterval = New Infragistics.Win.UltraWinSchedule.TimeInterval()
+        Dim DateInterval1 As Infragistics.Win.UltraWinSchedule.DateInterval = New Infragistics.Win.UltraWinSchedule.DateInterval()
+        Me.UltraCalendarLook1 = New Infragistics.Win.UltraWinSchedule.UltraCalendarLook(Me.components)
+        Me.UltraMonthViewMulti1 = New Infragistics.Win.UltraWinSchedule.UltraMonthViewMulti()
+        Me.UltraListBar1 = New Infragistics.Win.UltraWinListBar.UltraListBar()
+        Me.UltraStatusBar1 = New Infragistics.Win.UltraWinStatusBar.UltraStatusBar()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.UltraTimelineView1 = New Infragistics.Win.UltraWinSchedule.UltraTimelineView()
+        Me.UltraPanel1 = New Infragistics.Win.Misc.UltraPanel()
+        Me.ToolStripContainer1 = New System.Windows.Forms.ToolStripContainer()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripButton1 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripButton2 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripButton3 = New System.Windows.Forms.ToolStripButton()
+        CType(Me.UltraMonthViewMulti1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.UltraListBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        CType(Me.UltraTimelineView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.UltraPanel1.ClientArea.SuspendLayout()
+        Me.UltraPanel1.SuspendLayout()
+        Me.ToolStripContainer1.ContentPanel.SuspendLayout()
+        Me.ToolStripContainer1.SuspendLayout()
+        Me.ToolStrip1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'UltraMonthViewMulti1
+        '
+        Me.UltraMonthViewMulti1.BackColor = System.Drawing.SystemColors.Window
+        Me.UltraMonthViewMulti1.DayOfWeekCaptionStyle = Infragistics.Win.UltraWinSchedule.DayOfWeekCaptionStyle.FirstTwoLetters
+        Me.UltraMonthViewMulti1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.UltraMonthViewMulti1.Location = New System.Drawing.Point(0, 0)
+        Me.UltraMonthViewMulti1.MonthDimensions = New System.Drawing.Size(6, 1)
+        Me.UltraMonthViewMulti1.Name = "UltraMonthViewMulti1"
+        Me.UltraMonthViewMulti1.ResizeMode = Infragistics.Win.UltraWinSchedule.ResizeMode.BaseOnControlSize
+        Me.UltraMonthViewMulti1.Size = New System.Drawing.Size(1088, 130)
+        Me.UltraMonthViewMulti1.TabIndex = 0
+        '
+        'UltraListBar1
+        '
+        Me.UltraListBar1.Dock = System.Windows.Forms.DockStyle.Fill
+        Group1.Key = "a"
+        Group1.Text = "Casa dos Sabiás"
+        Group2.Key = "b"
+        Group2.Text = "Casa Igor"
+        Group3.Key = "c"
+        Group3.Text = "Casa Daniel"
+        Group4.Key = "d"
+        Group4.Text = "Sobrado Teresinha"
+        Group5.Key = "e"
+        Group5.Text = "Apartamento 31"
+        Me.UltraListBar1.Groups.Add(Group1)
+        Me.UltraListBar1.Groups.Add(Group2)
+        Me.UltraListBar1.Groups.Add(Group3)
+        Me.UltraListBar1.Groups.Add(Group4)
+        Me.UltraListBar1.Groups.Add(Group5)
+        Me.UltraListBar1.Location = New System.Drawing.Point(0, 0)
+        Me.UltraListBar1.Name = "UltraListBar1"
+        Me.UltraListBar1.Size = New System.Drawing.Size(333, 898)
+        '
+        'UltraStatusBar1
+        '
+        Me.UltraStatusBar1.Location = New System.Drawing.Point(0, 940)
+        Me.UltraStatusBar1.Name = "UltraStatusBar1"
+        Me.UltraStatusBar1.Size = New System.Drawing.Size(1425, 23)
+        Me.UltraStatusBar1.TabIndex = 4
+        Me.UltraStatusBar1.Text = "UltraStatusBar1"
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 42)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.UltraListBar1)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.UltraTimelineView1)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.UltraMonthViewMulti1)
+        Me.SplitContainer1.Size = New System.Drawing.Size(1425, 898)
+        Me.SplitContainer1.SplitterDistance = 333
+        Me.SplitContainer1.TabIndex = 6
+        '
+        'UltraTimelineView1
+        '
+        TimeInterval1.Interval = 12
+        TimeInterval1.IntervalUnits = Infragistics.Win.UltraWinSchedule.TimeIntervalUnits.Hours
+        Me.UltraTimelineView1.AdditionalIntervals.Add(TimeInterval1)
+        Me.UltraTimelineView1.ColumnWidth = 0
+        Me.UltraTimelineView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.UltraTimelineView1.Location = New System.Drawing.Point(0, 130)
+        Me.UltraTimelineView1.MaximumOwnersInView = 6
+        Me.UltraTimelineView1.MinimumRowResizeHeight = 30
+        Me.UltraTimelineView1.Name = "UltraTimelineView1"
+        Me.UltraTimelineView1.OwnerSizing = Infragistics.Win.UltraWinSchedule.TimelineViewOwnerSizing.Row
+        Me.UltraTimelineView1.PrimaryInterval = DateInterval1
+        Me.UltraTimelineView1.Size = New System.Drawing.Size(1088, 768)
+        Me.UltraTimelineView1.TabIndex = 4
+        '
+        'UltraPanel1
+        '
+        '
+        'UltraPanel1.ClientArea
+        '
+        Me.UltraPanel1.ClientArea.Controls.Add(Me.ToolStripContainer1)
+        Me.UltraPanel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.UltraPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.UltraPanel1.Name = "UltraPanel1"
+        Me.UltraPanel1.Size = New System.Drawing.Size(1425, 42)
+        Me.UltraPanel1.TabIndex = 3
+        '
+        'ToolStripContainer1
+        '
+        '
+        'ToolStripContainer1.ContentPanel
+        '
+        Me.ToolStripContainer1.ContentPanel.Controls.Add(Me.ToolStrip1)
+        Me.ToolStripContainer1.ContentPanel.Size = New System.Drawing.Size(1425, 42)
+        Me.ToolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ToolStripContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStripContainer1.Name = "ToolStripContainer1"
+        Me.ToolStripContainer1.Size = New System.Drawing.Size(1425, 42)
+        Me.ToolStripContainer1.TabIndex = 0
+        Me.ToolStripContainer1.Text = "ToolStripContainer1"
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripButton1, Me.ToolStripButton2, Me.ToolStripButton3})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(1425, 42)
+        Me.ToolStrip1.TabIndex = 0
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'ToolStripButton1
+        '
+        Me.ToolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButton1.Image = Global.CasasDePraia.My.Resources.Resources.Script_Load
+        Me.ToolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton1.Name = "ToolStripButton1"
+        Me.ToolStripButton1.Size = New System.Drawing.Size(23, 39)
+        Me.ToolStripButton1.Text = "ToolStripButton1"
+        '
+        'ToolStripButton2
+        '
+        Me.ToolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButton2.Image = Global.CasasDePraia.My.Resources.Resources.Script_Edit
+        Me.ToolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton2.Name = "ToolStripButton2"
+        Me.ToolStripButton2.Size = New System.Drawing.Size(23, 39)
+        Me.ToolStripButton2.Text = "ToolStripButton2"
+        '
+        'ToolStripButton3
+        '
+        Me.ToolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButton3.Image = Global.CasasDePraia.My.Resources.Resources.Script_Delete
+        Me.ToolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton3.Name = "ToolStripButton3"
+        Me.ToolStripButton3.Size = New System.Drawing.Size(23, 39)
+        Me.ToolStripButton3.Text = "ToolStripButton3"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1361, 963)
+        Me.ClientSize = New System.Drawing.Size(1425, 963)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Controls.Add(Me.UltraStatusBar1)
+        Me.Controls.Add(Me.UltraPanel1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.UltraMonthViewMulti1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.UltraListBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        CType(Me.UltraTimelineView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.UltraPanel1.ClientArea.ResumeLayout(False)
+        Me.UltraPanel1.ResumeLayout(False)
+        Me.ToolStripContainer1.ContentPanel.ResumeLayout(False)
+        Me.ToolStripContainer1.ContentPanel.PerformLayout()
+        Me.ToolStripContainer1.ResumeLayout(False)
+        Me.ToolStripContainer1.PerformLayout()
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents UltraCalendarLook1 As Infragistics.Win.UltraWinSchedule.UltraCalendarLook
+    Friend WithEvents UltraMonthViewMulti1 As Infragistics.Win.UltraWinSchedule.UltraMonthViewMulti
+    Friend WithEvents UltraListBar1 As Infragistics.Win.UltraWinListBar.UltraListBar
+    Friend WithEvents UltraStatusBar1 As Infragistics.Win.UltraWinStatusBar.UltraStatusBar
+    Friend WithEvents SplitContainer1 As System.Windows.Forms.SplitContainer
+    Friend WithEvents UltraPanel1 As Infragistics.Win.Misc.UltraPanel
+    Friend WithEvents ToolStripContainer1 As System.Windows.Forms.ToolStripContainer
+    Friend WithEvents ToolStrip1 As System.Windows.Forms.ToolStrip
+    Friend WithEvents ToolStripButton1 As System.Windows.Forms.ToolStripButton
+    Friend WithEvents ToolStripButton2 As System.Windows.Forms.ToolStripButton
+    Friend WithEvents ToolStripButton3 As System.Windows.Forms.ToolStripButton
+    Friend WithEvents UltraTimelineView1 As Infragistics.Win.UltraWinSchedule.UltraTimelineView
 
 End Class
--- CasasDePraia/CasasDePraia/Form1.vb
+++ CasasDePraia/CasasDePraia/Form1.vb
@@ -1,3 +1,87 @@
-﻿Public Class Form1
+﻿Imports System.Collections.Generic
+Imports Infragistics.Win
+Imports Infragistics.Win.UltraWinSchedule
+Imports System.Diagnostics
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As System.EventArgs) Handles Me.Load
+
+        Me.UltraTimelineView1.CalendarInfo.Owners.Add("a", "Casa dos Sabiás")
+        Me.UltraTimelineView1.CalendarInfo.Owners.Add("b", "Casa Igor")
+        Me.UltraTimelineView1.CalendarInfo.Owners.Add("c", "Casa Daniel")
+        Me.UltraTimelineView1.CalendarInfo.Owners.Add("d", "Sbrado Teresinha")
+        Me.UltraTimelineView1.CalendarInfo.Owners.Add("e", "Apartamento 31")
+
+        InitOwnerProps(Me.UltraTimelineView1)
+
+    End Sub
+
+    Private Sub UltraMonthViewMulti1_Click(sender As Object, e As System.EventArgs) Handles UltraMonthViewMulti1.Click
+
+        Me.UltraTimelineView1.CalendarInfo.SelectedDateRanges.Clear()
+        Me.UltraTimelineView1.CalendarInfo.SelectedDateRanges.Add(Today)
+        Me.UltraTimelineView1.CalendarInfo.ActiveDay = Me.UltraMonthViewMulti1.CalendarInfo.ActiveDay
+
+    End Sub
+
+    Public Sub InitOwnerProps(ByVal control As UltraTimelineView)
+
+        '  Hide the unassigned owner
+        control.CalendarInfo.Owners.UnassignedOwner.Visible = False
+
+        '  Make each owner appear as a separate row in the control
+        '  Make the header text run horizontally from left to right.
+        control.OwnerGroupingStyle = TimelineViewOwnerGroupingStyle.Separate
+        control.OwnerHeaderTextOrientation = New TextOrientationInfo(0, TextFlowDirection.Horizontal)
+
+        '  Make three owners visible initially.
+        control.MaximumOwnersInView = 3
+
+        '  Show owner headers.
+        control.OwnerHeadersVisible = DefaultableBoolean.True
+
+        '  Make the image size (32 x 32), and align the image with
+        '  the bottom of the header so it appears under the text.
+        control.OwnerHeaderImageVisible = True
+        control.OwnerHeaderImageSize = New Size(32, 32)
+        control.OwnerHeaderAppearance.ImageHAlign = HAlign.Center
+        control.OwnerHeaderAppearance.ImageVAlign = VAlign.Bottom
+
+        '  Activate the first visible owner use the ActiveOwnerHeaderAppearance
+        '  to customize the look.
+        control.ActiveOwner = control.CalendarInfo.VisibleOwners(0)
+        control.ActiveOwnerHeaderAppearance.BackColor = Color.White
+        control.ActiveOwnerHeaderAppearance.BackColor2 = Color.LightBlue
+        control.ActiveOwnerHeaderAppearance.BorderColor = Color.DarkBlue
+
+        '  Allow sizing of the owner rows only from the header
+        '  Impose a minimum resize height so that at least one
+        '  row of appointments can be displayed.
+        control.OwnerSizing = TimelineViewOwnerSizing.Header
+        control.MinimumRowResizeHeight = control.ActivityHeightResolved + 20
+        control.MaximumOwnersInView = 10
+        control.ActivityHeight = 50
+
+    End Sub
+
+    Private Sub UltraTimelineView1_ActiveOwnerChanged(sender As Object, e As Infragistics.Win.UltraWinSchedule.ActiveOwnerChangedEventArgs) Handles UltraTimelineView1.ActiveOwnerChanged
+
+        UltraListBar1.Groups.Item(e.NewActiveOwner.Key).Selected = True
+
+    End Sub
+
+    Private Sub UltraTimelineView1_MouseClick(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles UltraTimelineView1.MouseClick
+
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            Me.UltraMonthViewMulti1.CalendarInfo.SelectedDateRanges.Clear()
+            Me.UltraMonthViewMulti1.CalendarInfo.SelectedDateRanges.Add(Me.UltraTimelineView1.SelectedDateTimeRange.StartDateTime)
+            Me.UltraMonthViewMulti1.EnsureVisible(Me.UltraMonthViewMulti1.CalendarInfo.GetMonth(Me.UltraMonthViewMulti1.CalendarInfo.SelectedDateRanges.Item(0).StartDate.Date))
+
+        ElseIf e.Button = Windows.Forms.MouseButtons.Right Then
+
+        End If
+
+    End Sub
 
 End Class
